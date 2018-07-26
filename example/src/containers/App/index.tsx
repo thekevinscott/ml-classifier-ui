e6db21a9662f56a17de19d02c38a0f510b31bbf1
Kevin Scott <kevin@hitheory.com>
2018-07-26 08:51:03 -0400

    Add a link to datasets

--- example/src/containers/App/index.tsx
+++ example/src/containers/App/index.tsx
@@ -101,7 +101,10 @@ class App extends React.Component {
           {SHOW_HELP && this.state.training === false && (
             <div className={styles.info}>
               <h2>Instructions</h2>
-              <p>Drag and drop some labeled images below to begin training your classifier.</p>
+              <p>
+                Drag and drop some labeled images below to begin training your classifier.
+                <a href="https://thekevinscott.com/download-image-datasets-for-image-classification/">You can download image datasets here.</a>
+            </p>
               <p><em>Organize your images into folders, where the folders' names are the desired labels.</em></p>
               <div className={styles.imgContainer}>
                 <a target="_blank" href="https://github.com/thekevinscott/ml-classifier-ui/raw/master/example/public/example.gif">
